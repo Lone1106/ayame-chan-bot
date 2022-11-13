481559aeec50af0895fc9be49ef2483fe93b4cba
Lone1106 <97834596+Lone1106@users.noreply.github.com>
2022-11-13 15:22:18 +0100

    Added search for trending manga, refactored function to get trending content. Rewrote help messages.

--- reactions/help_me.py
+++ reactions/help_me.py
@@ -5,30 +5,31 @@ async def help_me(message):
 
     if m == "!ayame":
         await message.channel.send(
-            "To get help on channel specific commands please type '!help channel' in the corresponding channel. If you want to know about my reactions please use '!help reactions'. You can always check my readme here https://github.com/Lone1106/ayame-chan-bot"
-        )
-
-    if m == "!ayame channel" and c == "ayame-quote":
-        await message.channel.send(
-            "This is my quote channel. Write one of the following commands to get a anime quote from me. !ayame quote for a random quote, !ayame quote -a $[anime name] for a random quote from an anime, !ayame quote -c $[character name] for a random quote from your character. (Please dont use square brackets when you write!)"
-        )
-
-    if m == "!ayame channel" and c == "ayame-search":
-        await message.channel.send(
-            "Upload an image to the channel or link an image and I will search the anime and episode for you! If you use a link please perfix it with !ayame search $'LINK'. If you upload an image to the channel put !ayame search into the message of the image."
-        )
-
-    if m == "!ayame channel" and c == "ayame-waifus":
-        await message.channel.send(
-            "Type **!ayame waifu** into the ayame-waifus channel and I will send you a SFW image or gif of a random anime waifu out of preset categories."
-        )
-
-    if m == "!ayame channel" and c == "ayame-recommend":
-        await message.channel.send(
-            "(Type !ayame trending to get info of the latest trending anime from me!)"
+            "To get help on channel specific commands please type **!help channel** in any of my channels. If you want to know about my reactions please use **!help reactions**."
         )
 
     if m == "!ayame reactions":
         await message.channel.send(
-            "To make me react please write !ayame [reaction].All of my current reactions are dance, yo, cool, swarm, idol, angry, wondering, want, blink."
+            "To make me react please write **!ayame [reaction name]**. All of my current reactions are dance, yo, cool, swarm, idol, angry, wondering, want, blink."
         )
+
+    if m == "!ayame channel":
+        if c == "ayame-search":
+            await message.channel.send(
+                "Upload an image to the channel or link an image and I will search the anime and episode for you! If you use a link please perfix it with **!ayame search $[LINK]**. If you upload an image to the channel put **!ayame search** into the message of the image. (Please dont use square brackets for the link!)"
+            )
+
+        if c == "ayame-quote":
+            await message.channel.send(
+                "Write one of the following commands to get a quote from me. **!ayame quote** for a random quote, **!ayame quote -a $[anime name]** for a random quote from the named anime, **!ayame quote -c $[character name]** for a random quote from the named character. (Please dont use square brackets for the names!)"
+            )
+
+        if c == "ayame-waifus":
+            await message.channel.send(
+                "Type **!ayame waifu** into the ayame-waifus channel and I will send you a SFW image or gif of a random anime waifu out of preset categories."
+            )
+
+        if c == "ayame-recommend":
+            await message.channel.send(
+                "Type **!ayame trending -a** to get info of the latest trending anime from me and **!ayame trending -m** for manga!"
+            )
--- recommend/recommend.py
+++ recommend/recommend.py
@@ -8,22 +8,28 @@ headers = {
 
 async def get_recommend(message):
 
-    query = message.content.strip()
-
-    if query == "!ayame trending":
-
+    async def get_data(endpoint):
         try:
-            trend = requests.get("https://kitsu.io/api/edge/trending/anime",
-                                 headers=headers).json()
-            title = trend["data"][0]["attributes"]["canonicalTitle"]
-            anime_id = trend["data"][0]["id"]
-            desc = trend["data"][0]["attributes"]["synopsis"]
-            rating = trend["data"][0]["attributes"]["averageRating"]
+            trend = requests.get(endpoint, headers=headers).json()["data"][0]
+            cover = trend["attributes"]["coverImage"]["small"]
+            title = trend["attributes"]["canonicalTitle"]
+            anime_id = trend["id"]
+            desc = trend["attributes"]["synopsis"]
+            rating = trend["attributes"]["averageRating"]
 
             await message.channel.send(
-                f"Title: {title} \n ID: {anime_id} \n average Rating: {rating} \n Synopsis: {desc}\n"
+                f"Title: {title} \nID: {anime_id} \naverage Rating: {rating} \nSynopsis: {desc}\n {cover}"
             )
 
-        except Exception:
+        except Exception as e:
+            print(e)
             await message.channel.send(
                 "Hmmm hmm hmm, I couldn't find anything 人間.")
+
+    query = message.content.strip()
+
+    if query == "!ayame trending -a":
+        await get_data("https://kitsu.io/api/edge/trending/anime")
+
+    if query == "!ayame trending -m":
+        await get_data("https://kitsu.io/api/edge/trending/manga")
